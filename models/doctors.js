0ffad19568334f8ff9b006acfb3e209f4d7f3fd1
josephwojtowicz <69682505+josephwojtowicz@users.noreply.github.com>
2020-12-22 19:45:02 -0500

    add doctors.js

---
+++ models/doctors.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var doctor = {
+    all: function(cb) {
+        orm.all("doctors", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("doctors", cols, vals, function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("doctors", objColVals, condition, function(res){
+            cb(res);
+        });
+    }
+};
+
+module.exports = doctor;
